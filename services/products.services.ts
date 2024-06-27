fd83ed73aec93131bd63405e1f0bcd24462c274f
Wojciech <weswojciech@gmail.com>
2024-06-27 15:09:23 +0200

    fix to product services

--- services/products.services.ts
+++ services/products.services.ts
@@ -1,22 +1,47 @@
-import { Op, Sequelize } from "sequelize";
+import { Op, Order, Sequelize, WhereOptions } from "sequelize";
 import { Product, ProductDetails } from "../src/models/models";
 import { ApiError } from "../src/errors/ApiError";
 import {
+  ProductAttributes,
   ProductCreationAttributes,
   ProductDetailsCreationAttributes,
 } from "../src/types/types";
-export const getProducts = async (userId: number) => {
-  return await Product.findAll({
-    include: [
-      {
-        model: ProductDetails,
-        where: {
-          userId,
-        },
-      },
-    ],
+export async function getProducts(
+  limit?: number,
+  offset?: number,
+  order?: Order,
+  category?: string,
+  name?: string,
+) {
+  const where: WhereOptions<ProductAttributes> = {};
+  if (category) {
+    where.category = category;
+  }
+
+  if (name) {
+    where.name = {
+      [Op.iLike]: `%${name.toLowerCase()}%`,
+    };
+  }
+
+  const { rows, count } = await Product.findAndCountAll({
+    where,
+    limit,
+    offset,
+    order,
   });
-};
+  const totalPages = limit ? Math.ceil(count / limit) : 1;
+  const currentPage = limit && offset !== undefined ? offset / limit + 1 : 1;
+
+  return {
+    data: rows,
+    pagination: {
+      totalRecords: count,
+      totalPages,
+      currentPage,
+    },
+  };
+}
 
 export async function getProductDetails(id: string) {
   const details = await ProductDetails.findByPk(id, {
