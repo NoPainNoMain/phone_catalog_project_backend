819d8a8e0d5e710636b7e29d478608cc802f2031
Davwidos <d.jurkowski015@gmail.com>
2024-06-27 07:19:26 +0200

    added ctaegory query to products endpoit

--- services/products.services.ts
+++ services/products.services.ts
@@ -1,17 +1,44 @@
-import { Order } from "sequelize";
+import { Op, Order, WhereOptions } from "sequelize";
 import { Product, ProductDetails } from "../src/models/models";
 import { ApiError } from "../src/errors/ApiError";
+import { ProductAttributes } from "../src/types/types";
 
 export async function getProducts(
   limit?: number,
   offset?: number,
   order?: Order,
+  category?: string,
+  name?: string,
 ) {
-  return Product.findAll({
+  const where: WhereOptions<ProductAttributes> = {};
+  if (category) {
+    where.category = category;
+  }
+
+  if (name) {
+    where.name = {
+      [Op.iLike]: `%${name.toLowerCase()}%`,
+    };
+  }
+
+  const { rows, count } = await Product.findAndCountAll({
+    where,
     limit,
     offset,
     order,
   });
+
+  const totalPages = limit ? Math.ceil(count / limit) : 1;
+  const currentPage = limit && offset !== undefined ? offset / limit + 1 : 1;
+
+  return {
+    data: rows,
+    pagination: {
+      totalRecords: count,
+      totalPages,
+      currentPage,
+    },
+  };
 }
 
 export async function getProductDetails(id: string) {
